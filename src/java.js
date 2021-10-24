c505aac1b0b47b0ecae0f88b4b9bdfa023776696
nicolefrei8 <89259126+nicolefrei8@users.noreply.github.com>
2021-10-24 16:45:52 +1030

    CSS and JS updates

--- src/java.js
+++ src/java.js
@@ -1,1 +1,21 @@
+function displayTemperature(response) {
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.name;
+  let temp = document.querySelector("#temp");
+  temp.innerHTML = Math.round(response.data.main.temp);
+  let weatherDescription = document.querySelector("#weather-description");
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  let wind = document.querySelector("#wind");
+  let windSpeed = Math.round(response.data.wind.speed);
+  wind.innerHTML = `Wind: ${windSpeed}Km/H`;
+  let humidity = document.querySelector("#humidity");
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+}
 
+let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+let cityName = "Adelaide";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+
+axios.get(apiUrl).then(displayTemperature);
