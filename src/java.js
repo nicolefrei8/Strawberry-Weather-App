1cf1766fa71a749186de8fa072be35f1dc30c2ee
nicolefrei8 <89259126+nicolefrei8@users.noreply.github.com>
2021-10-26 22:58:31 +1030

    Added icon from API

--- src/java.js
+++ src/java.js
@@ -18,27 +18,38 @@ function formatDate(timestamp) {
     "Saturday",
   ];
   let day = days[date.getDay()];
-  return `Last updated at:  ${day} ${hours}:${minutes}`;
+  return `Last updated:  ${day} ${hours}:${minutes}`;
 }
 
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = response.data.name;
   let temp = document.querySelector("#temp");
-  temp.innerHTML = Math.round(response.data.main.temp);
   let weatherDescription = document.querySelector("#weather-description");
-  weatherDescription.innerHTML = response.data.weather[0].description;
   let wind = document.querySelector("#wind");
   let windSpeed = Math.round(response.data.wind.speed);
-  wind.innerHTML = `Wind: ${windSpeed}Km/H`;
   let humidity = document.querySelector("#humidity");
-  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   let dateElement = document.querySelector("#date-time");
+  let iconElement = document.querySelector("#icon");
+
+  cityElement.innerHTML = response.data.name;
+  temp.innerHTML = Math.round(response.data.main.temp);
+  weatherDescription.innerHTML = response.data.weather[0].description;
+  wind.innerHTML = `Wind: ${windSpeed}Km/H`;
+  humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  iconElement.setAttribute(
+    "src",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+  );
+  iconElement.setAttribute(
+    "alt",
+    `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
+  );
+
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
-let cityName = "Adelaide";
+let cityName = "melbourne";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
