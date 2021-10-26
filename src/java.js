d3f89a42003ef46ed125aa2eb0ba85bbf080ade9
nicolefrei8 <89259126+nicolefrei8@users.noreply.github.com>
2021-10-26 21:55:11 +1030

    incorporated api and date

--- src/java.js
+++ src/java.js
@@ -1,3 +1,26 @@
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `Last updated at:  ${day} ${hours}:${minutes}`;
+}
+
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.name;
@@ -10,12 +33,12 @@ function displayTemperature(response) {
   wind.innerHTML = `Wind: ${windSpeed}Km/H`;
   let humidity = document.querySelector("#humidity");
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
+  let dateElement = document.querySelector("#date-time");
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
 let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
 let cityName = "Adelaide";
 let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
-
-console.log(apiUrl);
 
 axios.get(apiUrl).then(displayTemperature);
