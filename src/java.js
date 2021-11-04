611c3f18b54374135882d56f1e35b11433b027d2
nicolefrei8 <89259126+nicolefrei8@users.noreply.github.com>
2021-11-04 23:02:26 +1030

    display forecast data using API

--- src/java.js
+++ src/java.js
@@ -20,66 +20,87 @@ function formatDate(timestamp) {
   let day = days[date.getDay()];
   return `Last updated:  ${day} ${hours}:${minutes}`;
 }
-function displayForecast() {
-  let forecastElement = document.querySelector("#weather-forecast");
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
 
+  return days[day];
+}
+
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#weather-forecast");
   let forecastHtml = `<div class="row">`;
-  let days = ["Thu", "Fri", "Sat"];
-  days.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `       <div class="col-2">
+
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHtml =
+        forecastHtml +
+        `
+             <div class="col-2">
                 <span class="forecast-day">
-                  ${day}
+                  ${formatDay(forecastDay.dt)}
                 </span>
                 <img 
-                src="https://ssl.gstatic.com/onebox/weather/48/sunny.png" 
+                src="http://openweathermap.org/img/wn/${
+                  forecastDay.weather[0].icon
+                }@2x.png"
                 alt=""
                 class="forecast-icon">
                 <span class="high-temp">
-                10
+                ${Math.round(forecastDay.temp.max)}°
                 </span>
                 <span class="low-temp">
-                  5
+                    ${Math.round(forecastDay.temp.min)}°
                 </span>
             </div>`;
+    }
   });
   forecastHtml = forecastHtml + `</div>`;
   forecastElement.innerHTML = forecastHtml;
+}
+function getForecast(coordinates) {
+  let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
   let cityElement = document.querySelector("#city");
   let temp = document.querySelector("#temp");
-  let weatherDescription = document.querySelector("#weather-description");
+  let weatherDescriptior = document.querySelector("#weather-description");
   let wind = document.querySelector("#wind");
   let windSpeed = Math.round(response.data.wind.speed);
   let humidity = document.querySelector("#humidity");
   let dateElement = document.querySelector("#date-time");
-  let iconElement = document.querySelector("#icon");
+  let iconElement = document.querySelector("#main-icon");
 
   celciusTemp = response.data.main.temp;
 
   cityElement.innerHTML = response.data.name;
   temp.innerHTML = Math.round(response.data.main.temp);
-  weatherDescription.innerHTML = response.data.weather[0].description;
+  weatherDescriptior.innerHTML = response.data.weather[0].description;
   wind.innerHTML = `Wind: ${windSpeed}Km/H`;
   humidity.innerHTML = `Humidity: ${response.data.main.humidity}%`;
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
-  weatherDescription.setAttribute(
+  weatherDescriptior.setAttribute(
     "alt",
     `http://openweathermap.org/img/wn/${response.data.weather[0].description}@2x.png`
   );
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
+
+  getForecast(response.data.coord);
 }
 function search(cityName) {
   let apiKey = "72bb9dab46b9ec3d65f423c63f27a9b8";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
-  console.log(apiUrl);
 }
 
 function searchClick(event) {
@@ -103,8 +124,6 @@ function convertCelcius(event) {
   fahrLink.classList.remove("active");
   temp.innerHTML = Math.round(celciusTemp);
 }
-
-displayForecast();
 
 let celciusTemp = null;
 
